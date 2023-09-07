6a261da06b8beb1b64dd6159f08ebc909f34c042
ragnieszka <agnieszka_roguska@proton.me>
2023-09-07 11:47:02 +0200

    generic dunctions for data filtering and columns renaming created

--- poc3.py
+++ poc3.py
@@ -1,45 +1,40 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-clients_path = '/mnt/c/users/aroguska/PySpark_UpSkill_v0_1_1/poc3/data/clients.csv'
-financial_path = '/mnt/c/users/aroguska/PySpark_UpSkill_v0_1_1/poc3/data/financial.csv'
+import functions
+
+working_directory = '/mnt/c/users/aroguska/PySpark_UpSkill_v0_1_1/poc3/data'
+
+clients_path = working_directory + '/clients.csv'
+financial_path = working_directory + '/financial.csv'
 
 spark = (SparkSession.builder
          .appName('poc')
          .getOrCreate()
          )
-#
+
 #BRONZE DATA - raw data read from the file 
 clients_DB = (spark
               .read
               .option('header', True)
               .option('delimiter', ',')
               .csv(clients_path)
-)
+              )
 
 financial_DB = (spark
               .read
               .option('header', True)
               .option('delimiter', ',')
               .csv(financial_path)
-)
+              )
 
 df = clients_DB.join(financial_DB, 'id')
 
+column_names_to_change = ['cc_t', 'cc_n', 'cc_mc', 'a', 'ac_t']
+column_names_new = ['credit_card_type', 'credit_card_number', 'credit_card_main_currency', 'active', 'account_type']
 
-column_pair_old_new = {'cc_t' : 'credit_card_type',
-                       'cc_n' : 'credit_card_number',
-                       'cc_mc' : 'credit_card_main_currency',
-                       'a' : 'active',
-                       'ac_t' : 'account_type'
-                       }
+df = functions.filter_countries(df, ['Poland', 'France'])
+df = functions.rename_columns(df, column_names_to_change, column_names_new)
+df = functions.remove_personal_identifiable_informations(df)
 
-mapping = dict(zip(['cc_t', 'cc_n', 'cc_mc', 'a', 'ac_t'], ['credit_card_type', 'credit_card_number', 'credit_card_main_currency', 'active', 'account_type']))
-"""for key, value in column_pair_old_new.items():
-    df = df.withColumnRenamed(key, value)"""
-
-df = (df
-      .filter(col('country').isin(['France', 'Poland']))
-      .select([col(x).alias(mapping.get(x, x)) for x in df.columns])
-      .drop('first_name', 'last_name', 'phone', 'birthdate', 'id')
-      )
+df.show()
