8cddd286c89951c9bebb6fb9fd3b53500a31e6f4
ragnieszka <agnieszka_roguska@proton.me>
2023-09-08 14:57:53 +0200

    remove_PII function slightly changed
    previously it was dropping `id` column, but now this action is done in the main program - poc3

--- functions.py
+++ functions.py
@@ -8,4 +8,4 @@ def rename_columns(dataframe, column_names_to_change, column_names_new):
     return dataframe.select([col(x).alias(mapping.get(x, x)) for x in dataframe.columns])
 
 def remove_personal_identifiable_informations(dataframe):
-    return dataframe.drop('first_name', 'last_name', 'phone', 'birthdate', 'id')
+    return dataframe.drop('first_name', 'last_name', 'phone', 'birthdate')
