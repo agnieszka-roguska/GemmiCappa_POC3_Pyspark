40a2b0a9c79d7f60a37fa202671583056d0d7415
ragnieszka <agnieszka_roguska@proton.me>
2023-09-13 13:25:32 +0200

    reformatted

--- poc3.py
+++ poc3.py
@@ -2,17 +2,9 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--clients_path", type = str, required = True, help = "path to clients dataset"
-    )
-parser.add_argument(
-    "--financial_path", type = str, required = True, help = "path to financial dataset"
-    )
-parser.add_argument(
-    "--countries_to_preserve", 
-    type = str, required = True, 
-    help = "list of countries we want to preserve in the final dataset; countries should be separated by commas (no spaces neither before nor after commas)"
-    )
+parser.add_argument("--clients_path", type = str, required = True, help = "path to clients dataset")
+parser.add_argument("--financial_path", type = str, required = True, help = "path to financial dataset")
+parser.add_argument("--countries_to_preserve", type = str, required = True, help = "list of countries we want to preserve in the final dataset; countries should be separated by commas (no spaces neither before nor after commas)")
 
 args = parser.parse_args()
 
@@ -21,7 +13,7 @@ financial_path = args.financial_path
 countries_to_preserve = args.countries_to_preserve.split(",")
 
 
-def main(clients_path: str, financial_path: str, list_of_countries_to_preserve: list):
+def main(clients_path : str, financial_path : str, list_of_countries_to_preserve : list):
 
     import re
     import logging
@@ -30,38 +22,46 @@ def main(clients_path: str, financial_path: str, list_of_countries_to_preserve: 
     from pyspark.sql.functions import col
 
     import functions
-
+ 
     FORMAT = "%(asctime)s:%(name)s:%(levelname)s - %(message)s"
     logging.basicConfig(format = FORMAT, level = logging.INFO)
 
-    spark = (SparkSession.builder.appName("poc").getOrCreate())
+    spark = (SparkSession.builder
+                    .appName("poc")
+                    .getOrCreate()
+                    )
 
-    # extracting working directory path from clients.csv file path
+    #extracting working directory path from clients.csv file path
     working_directory = re.sub(r"/clients.csv", "", clients_path)
 
-    # reading data from clients.csv and financial.csv files
+    #reading data from clients.csv and financial.csv files
     try:
-        clients_DB = (
-            spark.read.option("header", True)
-            .option("delimiter", ",")
-            .csv(clients_path)
-            )
+        clients_DB = (spark
+                    .read
+                    .option("header", True)
+                    .option("delimiter", ",")
+                    .csv(clients_path)
+                    )
         logging.info("Clients data was correctly extracted from the file.")
     except: 
         logging.critical("Unable to load data from clients.csv file")
 
     try:
-        financial_DB = (
-            spark.read.option("header", True)
-            .option("delimiter", ",")
-            .csv(financial_path)
-            )
+        financial_DB = (spark
+                    .read
+                    .option("header", True)
+                    .option("delimiter", ",")
+                    .csv(financial_path)
+                    )
         logging.info("Financial data was correctly extracted from the file.")
     except: 
         logging.critical("Unable to load data from financial.csv file")
 
-    # creating new dataframe containing data from clients and financial files
-    df = clients_DB.join(financial_DB, "id").drop("id")
+    #creating new dataframe containing data from clients and financial files
+    df = (clients_DB
+          .join(financial_DB, "id")
+          .drop("id")
+          )
     
     column_names_to_change_old_new_pairs = {
         "cc_t" : "credit_card_type",
@@ -72,26 +72,24 @@ def main(clients_path: str, financial_path: str, list_of_countries_to_preserve: 
     }
     columns_with_PII = ["first_name", "last_name", "phone", "birthdate"]
 
-    # filtering out all clients from countries other than specified, removing the PPI and renaming columns as requested in a task
+    #filtering out all clients from countries other than specified, removing the PPI and renaming columns as requested in a task
     df = functions.filter_column(df, "country",  list_of_countries_to_preserve)
-    logging.info(
-        "Successfully filtered out customers from countries other than: %s",
-          list_of_countries_to_preserve
-          )
+    logging.info("Successfully filtered out customers from countries other than: %s", list_of_countries_to_preserve)
     df = functions.remove_personal_identifiable_information(df, columns_with_PII)
-    logging.info(
-        "Sucessfully removed all columns with personal identifiable information "
-        )
+    logging.info("Sucessfully removed all columns with personal identifiable information ")
     df = functions.rename_columns(df, column_names_to_change_old_new_pairs)
     logging.info("Successfully renamed abbreviated column names.")
 
-    # writing data to the parquet file
+    #writing data to the parquet file
     try:
-        (df.write.mode("overwrite").parquet(working_directory + "/client_data"))
+        (df
+        .write
+        .mode("overwrite")
+        .parquet(working_directory + "/client_data")
+        )
         logging.info("Data was successfully written to a file. All done!")
     except:
         logging.error("Unable to write data to a file.")
-        
 
 
 main(clients_path, financial_path, countries_to_preserve)
